d6ca6ec9810ebba1b43dadd90479b4b99db73d13
jakkasiva <jakkasivakumar2020@gmail.com>
2022-09-29 23:38:03 +0100

    added resources

--- main/main.tf
+++ main/main.tf
@@ -1,13 +1,19 @@
-provider "azurerm" {
+provider "azurerm"{
     features {}
 }
 
-module "resource_group" {
-  source = "../modules/rg"
+module "resource_group"{
+    source = "../modules/resourcegroup"
 }
 
 module "storage_account" {
-  source = "../modules/st"
+  source = "../modules/storageaccount"
   depends_on          = [module.resource_group]
-  resource_group_name = module.resource_group.resource_group_name
+  resource_group_name = module.resource_group.resource_group_name          
+}
+
+module "function_app" {
+  source = "../modules/functionapp"
+  depends_on          = [module.storage_account]
+  resource_group_name = module.resource_group.resource_group_name          
 }
